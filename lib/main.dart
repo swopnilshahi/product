90dd3726e94c321e2b3eea3c1cd6745b78434c77
Swopnil Shahi <swopnilshahi8@gmail.com>
2024-08-17 03:08:27 +0545

    setting of main

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Products',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColorLight: Colors.white,
+        brightness: Brightness.dark,
+        canvasColor: Colors.blueGrey.shade50,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.grey,
+        brightness: Brightness.dark,
       ),
       home: const Home(),
     );
